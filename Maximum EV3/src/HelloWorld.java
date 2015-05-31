456af3783473a34dad8406220da98ddb3c35177e
Nishant Gaidhani <nishant.gaidhani@gmail.com>
2015-05-31 22:31:16 +0530

    first

---
+++ Maximum EV3/src/HelloWorld.java
@@ -0,0 +1,10 @@
+import lejos.hardware.*;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Button.waitForAnyPress();
+	}
+
+}
